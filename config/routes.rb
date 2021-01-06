5956086acc35f628d1e5bf98926da959ccd25015
leska91 <mayu1101u@gmail.com>
2021-01-06 21:22:59 +0900

    topページ表示のためitemsコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  root to: "items#index"
   get '/users/sign_up', to: 'registrations#new'
+  get '/users/sign_in', to: 'sessions#new'
+  get '/users/sign_out', to: 'sessions#destroy'
 end
