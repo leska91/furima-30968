cbe33625ae72227b27da24c281860ace53a8dc3c
leska91 <mayu1101u@gmail.com>
2021-01-08 11:17:51 +0900

    モデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name_kanji,  presence: true
-  validates :first_name_kanji,  presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana,  presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は英数字混合にしてください'
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角文字を使用してください' } do
+    validates :last_name_kanji
+    validates :first_name_kanji
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
   validates :birthday,  presence: true
 
   has_many :items
