ecb2ee96a9cc715adb8ba4ab6bf6fc17bf3bdfe7
leska91 <mayu1101u@gmail.com>
2021-01-11 12:02:26 +0900

    商品出品ページを表示できるようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,23 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if  @item.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :information, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :sell_price).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:index, :new]
+  resources :items, only: [:new, :create]
   get '/users/sign_up', to: 'registrations#new'
   get '/users/sign_in', to: 'sessions#new'
   get '/users/sign_out', to: 'sessions#destroy'
