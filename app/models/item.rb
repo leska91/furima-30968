c3975e23ca3215384f221c70ec2cbb1e413d72bc
leska91 <mayu1101u@gmail.com>
2021-01-11 14:09:47 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_fee_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
+
+  with_options presence: true do
+    validates :name
+    validates :information
+  end
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :sales_status_id, numericality: { other_than: 1 }
+  validates :shipping_fee_status_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :scheduled_delivery_id, numericality: { other_than: 1 }
 end
