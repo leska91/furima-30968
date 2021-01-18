5a22e479f0e417a0c7764b5a8777940899e35d59
leska91 <mayu1101u@gmail.com>
2021-01-18 17:50:19 +0900

    出品者でないユーザーがdestroyアクションを行った際の情報分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,8 @@ class ItemsController < ApplicationController
     if current_user.id == @item.user_id
       @item.destroy
       redirect_to action: :index
+    else
+      render :index
     end
   end
 
