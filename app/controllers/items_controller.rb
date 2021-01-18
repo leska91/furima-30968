ed1905211d095a6af571f8ea00b21b224cbb0c45
leska91 <mayu1101u@gmail.com>
2021-01-18 14:48:34 +0900

    rubocop実行・修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless current_user.id == @item.user_id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def update
@@ -44,5 +42,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :information, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id,
                                  :scheduled_delivery_id, :sell_price, :image).merge(user_id: current_user.id)
   end
-  
 end
