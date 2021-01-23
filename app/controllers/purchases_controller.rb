f68178ad6c2ed0918fed99d2d34011026a6c428b
leska91 <mayu1101u@gmail.com>
2021-01-23 10:19:56 +0900

    フォームの情報を保存できるようにする

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,5 +5,17 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase_shippinginfo = PurchaseShippinginfo.new(purchase_params)
+    if @purchase_shippinginfo.valid?
+      @purchase_shippinginfo.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_shippinginfo).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id] )
   end
 end
--- app/models/purchase_shippinginfo.rb
+++ app/models/purchase_shippinginfo.rb
@@ -1,6 +1,6 @@
 class PurchaseShippinginfo
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
@@ -9,11 +9,11 @@ class PurchaseShippinginfo
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "は半角数字ハイフンなしで入力してください" }
   end
 
-  validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
-    Purchase.create(user_id: user.id, item_id: item.id)
-    Shippinginfo.create(postal_code: postal_code, prefecture: prefecture, city: city, address: address, building: building, phone_number: phone_number, purchase_id: purchase.id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    ShippingInfo.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 
 end
