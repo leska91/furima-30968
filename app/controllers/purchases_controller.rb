a85a2e73ac3778d7e440767167907cc10001b0e7
leska91 <mayu1101u@gmail.com>
2021-01-25 11:54:52 +0900

    売却済み商品のアクション記述追加

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -38,6 +38,6 @@ class PurchasesController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:item_id])
-    return redirect_to root_path if current_user.id == @item.user_id
+    return redirect_to root_path if (current_user.id == @item.user_id) || @item.purchase.present?
   end
 end
